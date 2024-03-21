244e062e3c8ddbff882fa73cc0ececc55ba4d19f
gustavom2998 <42518882+gustavom2998@users.noreply.github.com>
2024-03-21 11:21:08 -0300

    add init config

---
+++ makefile
@@ -0,0 +1,4 @@
+import:
+	rm -rf ~/.zsh
+	rm -rf ~/.zshrc
+	cp zshrc ~/.zshrc	
---
+++ theme.zsh
@@ -0,0 +1,2 @@
+# eval "$(oh-my-posh init zsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubblesline.omp.json')"
+eval "$(oh-my-posh init zsh --config '~/zsh/theme.omp.json')"
